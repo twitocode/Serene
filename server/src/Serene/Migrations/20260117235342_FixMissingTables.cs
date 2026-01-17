9ca55d49d5b68702ff84d829e53fd79dfa0ff47f
twitocode <toheebeji@gmail.com>
2026-01-17 18:54:17 -0500

    Add missing migration for community_qotd table

---
+++ server/src/Serene/Migrations/20260117235342_FixMissingTables.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Pgvector;
+
+#nullable disable
+
+namespace Serene.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixMissingTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Vector>(
+                name: "embedding",
+                table: "explore_content",
+                type: "vector(1024)",
+                nullable: true,
+                oldClrType: typeof(Vector),
+                oldType: "vector",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Vector>(
+                name: "embedding",
+                table: "explore_content",
+                type: "vector",
+                nullable: true,
+                oldClrType: typeof(Vector),
+                oldType: "vector(1024)",
+                oldNullable: true);
+        }
+    }
+}
