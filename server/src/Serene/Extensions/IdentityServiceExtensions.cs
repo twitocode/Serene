2fb7fea5dd5b12c942fbe98828412c30b73ffe36
twitocode <toheebeji@gmail.com>
2026-01-17 20:42:11 -0500

    maybe claude to the rescue

--- server/src/Serene/Extensions/IdentityServiceExtensions.cs
+++ server/src/Serene/Extensions/IdentityServiceExtensions.cs
@@ -23,8 +23,8 @@ public static class IdentityServiceExtensions
         .AddCookie("ExternalCookie", o =>
         {
             o.Cookie.Name = ".Serene.External";
-            o.Cookie.Path = "/";
-            o.Cookie.SameSite = SameSiteMode.None;
+            o.Cookie.Path = "/api";
+            o.Cookie.SameSite = SameSiteMode.Lax;
             o.Cookie.SecurePolicy = CookieSecurePolicy.Always;
             o.Cookie.HttpOnly = true;
         })
@@ -32,7 +32,7 @@ public static class IdentityServiceExtensions
         {
             options.Authority = jwtOptions.Authority;
             options.Audience = jwtOptions.Audience;
-            options.RequireHttpsMetadata = false; 
+            options.RequireHttpsMetadata = false;
             options.TokenValidationParameters = new TokenValidationParameters
             {
                 ValidateIssuer = true,
@@ -64,9 +64,10 @@ public static class IdentityServiceExtensions
             o.Scope.Add("profile");
             o.SignInScheme = "ExternalCookie";
             o.CorrelationCookie.Name = ".Serene.Correlation";
-            o.CorrelationCookie.Path = "/";
-            o.CorrelationCookie.SameSite = SameSiteMode.None;
+            o.CorrelationCookie.Path = "/api";
+            o.CorrelationCookie.SameSite = SameSiteMode.Lax;
             o.CorrelationCookie.SecurePolicy = CookieSecurePolicy.Always;
+            o.CallbackPath = "/signin-google";
         });
 
         return services;
